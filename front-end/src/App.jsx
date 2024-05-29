dce9814343773787709da205216f31d1003d02b5
Joe Anderson <and10287@umn.edu>
2024-05-28 23:32:18 -0500

    Created Home, Game, Results components

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,10 +1,23 @@
 import './App.css'
+import React, {useState, createContext} from 'react';
+import Home from './components/Home';
+import Game from './components/Game';
+
+export const HighScoreContext = createContext();
+export const GameStartedContext = createContext();
 
 function App() {
-  return (
-    <>
+  const [highScore, setHighScore] = useState(0);
+  const [gameStarted, setGameStarted] = useState(false);
+  
 
-    </>
+  return (
+      <HighScoreContext.Provider value={highScore} >
+        <GameStartedContext.Provider value={gameStarted}>
+          {gameStarted ? <Game />
+                      : <Home />}
+        </GameStartedContext.Provider>   
+      </HighScoreContext.Provider>
   );
 }
 
