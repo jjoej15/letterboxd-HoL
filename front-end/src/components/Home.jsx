0d67844fa5cbf0ff0cc42072eddbb16d9e4275e7
Joe Anderson <and10287@umn.edu>
2024-05-29 02:36:11 -0500

    Created html for Home page

--- front-end/src/components/Home.jsx
+++ front-end/src/components/Home.jsx
@@ -1,7 +1,20 @@
+import Footer from "./Footer";
 
 function Home() {
     return(
-        <>Home</>
+        <>
+            <div className="Home">
+                <div className="vertical-center">
+                    <img type='image/svg+xml' alt='letterboxd decal dots' src='https://a.ltrbxd.com/logos/letterboxd-decal-dots-pos-rgb.svg'/>
+                    <h1 className="app-title">Letterboxd Higher or Lower</h1>
+                    <button className="play-game-btn">Play</button>
+                    <h3 className="high-score-home">High Score: 10</h3>
+                </div>
+            </div>
+
+            <Footer />
+        </>
+
     );
 }
 
