3d17a019062081aa479af6f690d6cbddb2361ccc
Joe Anderson <and10287@umn.edu>
2024-05-29 02:35:43 -0500

    Created Footer component

---
+++ front-end/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+
+function Footer() {
+    return(
+        <footer className="Footer">
+            <p>Project by <a href="https://github.com/jjoej15">Joe Anderson</a>. 
+            Check out the code <a href="https://github.com/jjoej15/letterboxd-HoL">here</a>.</p>
+        </footer>        
+    );
+}
+
+export default Footer;
