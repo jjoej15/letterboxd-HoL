c3c0cd2f198211cd52604ced2b50276a54512f21
Joe Anderson <and10287@umn.edu>
2024-05-29 23:01:35 -0500

    Changed anchor tag targets

--- front-end/src/components/Footer.jsx
+++ front-end/src/components/Footer.jsx
@@ -2,8 +2,8 @@
 function Footer() {
     return(
         <footer className="Footer">
-            <p>Project by <a href="https://github.com/jjoej15">Joe Anderson</a>. 
-            Check out the code <a href="https://github.com/jjoej15/letterboxd-HoL">here</a>.</p>
+            <p>Project by <a href="https://github.com/jjoej15" target="_blank">Joe Anderson</a>. 
+            Check out the code <a href="https://github.com/jjoej15/letterboxd-HoL" target="_blank">here</a>.</p>
         </footer>        
     );
 }
