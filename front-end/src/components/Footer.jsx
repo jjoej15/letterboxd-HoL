90a413bd5af244f3da5a361109ee884592b7edbe
Joe Anderson <and10287@umn.edu>
2024-05-31 19:41:57 -0500

    Changed footer text

--- front-end/src/components/Footer.jsx
+++ front-end/src/components/Footer.jsx
@@ -3,7 +3,7 @@ function Footer() {
     return(
         <footer className="Footer">
             <p>Project by <a href="https://github.com/jjoej15" target="_blank">Joe Anderson</a>. 
-            Check out the code <a href="https://github.com/jjoej15/letterboxd-HoL" target="_blank">here</a>.</p>
+            Data up to date as of May 2024.</p>
         </footer>        
     );
 }
