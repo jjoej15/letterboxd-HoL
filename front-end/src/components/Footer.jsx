427dd4084b23860eecea4f7495e4368ff00f267f
Joe Anderson <and10287@umn.edu>
2024-06-03 22:52:38 -0500

    Data update

--- front-end/src/components/Footer.jsx
+++ front-end/src/components/Footer.jsx
@@ -3,7 +3,7 @@ function Footer() {
     return(
         <footer className="Footer">
             <p>Project by <a href="https://github.com/jjoej15" target="_blank">Joe Anderson</a>. 
-            Data up to date as of May 2024.</p>
+            Data up to date as of June 2024.</p>
         </footer>        
     );
 }
